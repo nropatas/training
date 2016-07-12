bd006adcecf915744e752b35a4da24e06eee1b35
Sam Sataponn <sam@abctech-thailand.com>
2016-07-12 18:32:15 +0700

    Minify style.css

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,9 @@ const mocha = require('gulp-mocha');
 const gulputil = require('gulp-util');
 const nodemon = require('gulp-nodemon');
 const concatCss = require('gulp-concat-css');
+const cleanCss = require('gulp-clean-css');
+const sequence = require('gulp-sequence');
+const cssnano = require('gulp-cssnano');
 
 gulp.task('lint', () => {
     return gulp.src(['**/*.js', '!node_modules/**', '!public/**/*.js'])
@@ -20,7 +23,15 @@ gulp.task('concatCss', () => {
         .pipe(gulp.dest('public/css'));
 });
 
-gulp.task('run', ['lint', 'concatCss'], () => {
+gulp.task('minify', () => {
+    gulp.src('public/css/style.css')
+        .pipe(cssnano())
+        .pipe(gulp.dest('public/css'));
+});
+
+gulp.task('clean', sequence('concatCss', 'minify'));
+
+gulp.task('run', ['lint', 'clean'], () => {
     nodemon({
         script: 'app.js',
     });
