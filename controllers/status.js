84d2972ec1f337b9027d31f206c2a5147f23cbb0
Sam Sataponn <sam@abctech-thailand.com>
2016-07-14 13:50:51 +0700

    Refactor

--- controllers/status.js
+++ controllers/status.js
@@ -2,40 +2,41 @@
 
 const model = require('../models/status');
 const https = require('https');
+let status;
 
-module.exports = {
-    insert: (req, out) => {
-        let url = 'https://status.github.com/api/status.json';
+status.prototype.insert = (req, out) => {
+    let url = 'https://status.github.com/api/status.json';
 
-        https.get(url, (res) => {
-            res.on('data', (chunk) => {
-                let json = JSON.parse(chunk);
+    https.get(url, (res) => {
+        res.on('data', (chunk) => {
+            let json = JSON.parse(chunk);
 
-                model.insertNewStatus(json)
-                    .then(() => {
-                        out.write('A new status is recorded.');
-                        out.end();
-                    })
-                    .catch(console.error);
-            });
+            model.insertNewStatus(json)
+                .then(() => {
+                    out.write('A new status is recorded.');
+                    out.end();
+                })
+                .catch(console.error);
         });
-    },
-
-    logJson: (req, res) => {
-        model.getLog()
-            .then((rows) => {
-                res.type('application/json');
-                res.write(JSON.stringify(rows));
-                res.end();
-            })
-            .catch(console.error);
-    },
-
-    logHtml: (req, res) => {
-        model.getLog()
-            .then((rows) => {
-                res.render('log', { data: rows });
-            })
-            .catch(console.error);
-    },
+    });
 };
+
+status.prototype.logJson = (req, res) => {
+    model.getLog()
+        .then((rows) => {
+            res.type('application/json');
+            res.write(JSON.stringify(rows));
+            res.end();
+        })
+        .catch(console.error);
+};
+
+status.prototype.logHtml = (req, res) => {
+    model.getLog()
+        .then((rows) => {
+            res.render('log', { data: rows });
+        })
+        .catch(console.error);
+};
+
+module.exports = status;
--- controllers/users.js
+++ controllers/users.js
@@ -1,23 +1,24 @@
 'use strict';
 
 const model = require('../models/users');
+let users;
 
-module.exports = {
-    resJson: (req, res) => {
-        model.getUsers(req.params.role)
-            .then((rows) => {
-                res.type('application/json');
-                res.write(JSON.stringify(rows));
-                res.end();
-            })
-            .catch(console.error);
-    },
-
-    resHtml: (req, res) => {
-        model.getUsers(req.params.role)
-            .then((rows) => {
-                res.render('users', { data: rows });
-            })
-            .catch(console.error);
-    },
+users.prototype.resJson = (req, res) => {
+    model.getUsers(req.params.role)
+        .then((rows) => {
+            res.type('application/json');
+            res.write(JSON.stringify(rows));
+            res.end();
+        })
+        .catch(console.error);
 };
+
+users.prototype.resHtml = (req, res) => {
+    model.getUsers(req.params.role)
+        .then((rows) => {
+            res.render('users', { data: rows });
+        })
+        .catch(console.error);
+};
+
+module.exports = users;
